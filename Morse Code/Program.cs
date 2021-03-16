333b9d6ce5c8e5ec0caa9ce6e8845954e9763615
KuratovIgor <kuratovi@bk.ru>
2021-03-16 22:03:34 +0700

    Update with dictionaries

--- Morse Code/Program.cs
+++ Morse Code/Program.cs
@@ -9,23 +9,46 @@ namespace Morse_Code
         {
             String stringLetter;
 
-            Console.WriteLine("Choose Morse/Leter? [m/l] ");
-            char answer = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Choose language: [r,e] ");
+            char language = Convert.ToChar(Console.ReadLine());
 
-            switch (answer) {
-                case 'm': {
-                        Console.Write("Enter morse-code: ");
-                        stringLetter = Console.ReadLine();
-                        Console.WriteLine(MorseCode.TranslateToLetter(stringLetter));
-                }break;
-                case 'l': {
-                        Console.Write("Enter letter: ");
-                        stringLetter = Console.ReadLine();
-                        Console.WriteLine(MorseCode.TranslateToMorse(stringLetter));
-                }break;
-                default: {
-                        Console.WriteLine("You can choose only m or l! ");
-                }break;
+            if (language != 'e' && language != 'r')
+            {
+                Console.WriteLine("You can choose only r or e!");
+            }    
+            else
+            {
+                Console.WriteLine("Choose Morse/Leter? [m/l] ");
+                char answer = Convert.ToChar(Console.ReadLine());
+
+                switch (answer)
+                {
+                    case 'm':
+                        {
+                            Console.Write("Enter morse-code: ");
+                            stringLetter = Console.ReadLine();
+                            if (language == 'e')
+                                Console.WriteLine(MorseCode.TranslateToEnLetter(stringLetter));
+                            if (language == 'r')
+                                Console.WriteLine(MorseCode.TranslateToRusLetter(stringLetter));
+                        }
+                        break;
+                    case 'l':
+                        {
+                            Console.Write("Enter letter: ");
+                            stringLetter = Console.ReadLine();
+                            if (language == 'e')
+                                Console.WriteLine(MorseCode.TranslateToMorseEn(stringLetter));
+                            if (language == 'r')
+                                Console.WriteLine(MorseCode.TranslateToMorseRus(stringLetter));
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("You can choose only m or l!");
+                        }
+                        break;
+                }
             }
         }
     }
